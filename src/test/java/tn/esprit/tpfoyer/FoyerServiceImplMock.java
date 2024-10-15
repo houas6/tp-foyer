4bd9192e29402f91f40c884e58bf21698d5ad467
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-15 22:20:03 +0100

    Update FoyerServiceImplMock.java

--- src/test/java/tn/esprit/tpfoyer/FoyerServiceImplMock.java
+++ src/test/java/tn/esprit/tpfoyer/FoyerServiceImplMock.java
@@ -1,12 +1,7 @@
 package tn.esprit.tpfoyer.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.tpfoyer.repository.FoyerRepository;
 
 import static org.mockito.Mockito.*;
